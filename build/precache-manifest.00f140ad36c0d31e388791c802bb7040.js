ae64ed9b64769d5a4a2f2cf412936c9431522255
venu <venu@localhost.localdomain>
2019-02-22 18:53:36 +0530

    Initial Commit1

---
+++ build/precache-manifest.00f140ad36c0d31e388791c802bb7040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "73cb308043871fc979ae",
+    "url": "/contact-manager/static/js/main.73cb3080.chunk.js"
+  },
+  {
+    "revision": "f8efd8ff0e55710f1b8c",
+    "url": "/contact-manager/static/js/1.f8efd8ff.chunk.js"
+  },
+  {
+    "revision": "73cb308043871fc979ae",
+    "url": "/contact-manager/static/css/main.e00d141c.chunk.css"
+  },
+  {
+    "revision": "f8efd8ff0e55710f1b8c",
+    "url": "/contact-manager/static/css/1.3423caec.chunk.css"
+  },
+  {
+    "revision": "a50f2880f1058fa32c97dbbe91440229",
+    "url": "/contact-manager/index.html"
+  }
+];
