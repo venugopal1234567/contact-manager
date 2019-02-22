d03fde2bef692ba6a7929a54abc41c6433e0984d
venu <venu@localhost.localdomain>
2019-02-22 18:26:38 +0530

    Updates

---
+++ precache-manifest.7cebcc7cc2cb13e6054c7dac40ec9190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90a5ae7c49d09dc27c53",
+    "url": "/github.io/contactmanager/static/js/runtime~main.90a5ae7c.js"
+  },
+  {
+    "revision": "252df37d44264c025e00",
+    "url": "/github.io/contactmanager/static/js/main.252df37d.chunk.js"
+  },
+  {
+    "revision": "f8efd8ff0e55710f1b8c",
+    "url": "/github.io/contactmanager/static/js/1.f8efd8ff.chunk.js"
+  },
+  {
+    "revision": "252df37d44264c025e00",
+    "url": "/github.io/contactmanager/static/css/main.e00d141c.chunk.css"
+  },
+  {
+    "revision": "f8efd8ff0e55710f1b8c",
+    "url": "/github.io/contactmanager/static/css/1.3423caec.chunk.css"
+  },
+  {
+    "revision": "a30715dde5b7c1783c511cae2797435a",
+    "url": "/github.io/contactmanager/index.html"
+  }
+];
